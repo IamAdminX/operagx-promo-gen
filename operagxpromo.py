92e434ac12ff4e1a333b80c1a4f964ba78f0e379
AdminX <97702057+IamAdminX@users.noreply.github.com>
2023-12-19 18:08:21 +0100

    Add files via upload

---
+++ operagxpromo.py
@@ -0,0 +1,29 @@
+import requests, random, string, threading
+
+def rstr(l):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(l))
+
+def generate():
+    headers = { #that is literally all headers u need lmao except for the user agent
+        "content-type": "application/json",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0 (Edition std-1)"
+    }
+
+    payload = {
+        "partnerUserId": f"{rstr(8)}-{rstr(4)}-{rstr(4)}-{rstr(4)}-{rstr(12)}",
+    }
+
+    resp = requests.post("https://api.discord.gx.games/v1/direct-fulfillment", headers=headers, json=payload).json()
+    link = (f"https://discord.com/billing/partner-promotions/1180231712274387115/{resp['token']}")
+    print(link)
+    with open("promos.txt", "a") as f:
+        f.write(f"{link}\n")
+    return link
+
+def start():
+    while True:
+        generate()
+
+if __name__ == "__main__":
+    for i in range(100):
+        threading.Thread(target=start).start()
